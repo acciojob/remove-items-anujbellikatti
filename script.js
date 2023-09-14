c0380c9f0a3391078e779d67b5d415cc6ecd1046
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-09-15 01:32:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+// const dropDown = document.querySelector("#colorSelect");
+// const btn = document.getElementsByTagName("input")[0];
+// btn.addEventListener("click",() => {
+// let option = document.getElementsByTagName("option");
+// 	for(let i=0;i<option.length;i++)
+// 		if(option[i].innerText === dropDown.value)
+// 			return dropDown.removeChild(option[i]);
+// });
+
 const dropDown = document.querySelector("#colorSelect");
-const btn = document.getElementsByTagName("input")[0];
-btn.addEventListener("click", () => {
-  let option = document.getElementsByTagName("option");
-  for (let i = 0; i < option.length; i++)
-    if (option[i].innerText === dropDown.value)
-      return dropDown.removeChild(option[i]);
-});
+function removeElement(){
+	let cv = dropDown.value;
+	dropDown.remove(cv)	
+}
