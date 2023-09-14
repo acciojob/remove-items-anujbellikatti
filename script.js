ff68209a5d8b40e0888809c799e2a78ff670cc89
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-09-15 01:03:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let element = document.getElementById("colorSelect");
+
+function removeElement() {
+
+    let currentValue = element.value;
+    element.remove(currentValue);
+}
